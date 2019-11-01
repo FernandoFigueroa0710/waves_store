d8333cdb050848bb5cf8820486b896df5ba20d6e
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-11-01 01:53:58 -0600

    Users update Succesfully login, authenticate & logout responses 200

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 const SALT_I = 10;
 const userSchema = mongoose.Schema({
   email: {
@@ -58,21 +58,33 @@ userSchema.pre("save", function(next) {
   }
 });
 
-userSchema.methods.comparePassword = function (candidatePassword, cb) {
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) { return cb(err); }
+    if (err) {
+      return cb(err);
+    }
 
     cb(null, isMatch);
   });
 };
 
-userSchema.methods.generateToken = function(cb){
+userSchema.methods.generateToken = function(cb) {
   var user = this;
   var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
   user.token = token;
-  user.save(function(err, user){
-    if(err) return cb(err);
+  user.save(function(err, user) {
+    if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+  jwt.verify(token, process.env.SECRET, function(err, decode) {
+    user.findOne({ _id: decode, token: token }, function(err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 const User = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const { User } = require("./models/user");
+const { auth } = require("./middleware/auth");
 
 const app = express();
 require("dotenv").config();
@@ -16,6 +17,19 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 //********USERS*****************//
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastName: req.user.lastName,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+  });
+});
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
@@ -49,7 +63,14 @@ app.post("/api/users/login", (req, res) => {
     });
   });
 });
-
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: " " }, (err, doc) => {
+    if (err) return res.json({ sucess: false, err });
+    return res.status(200).send({
+      sucess: true
+    });
+  });
+});
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
