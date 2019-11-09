63d1c350233f1b5ea2bccbe05bf8e7fc539b2b20
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-11-08 22:27:52 -0600

    Update
    Header and footer created

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Layout from "./hoc/layout";
 import Home from "./components/Home/index";
+
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
   );
 };
 
