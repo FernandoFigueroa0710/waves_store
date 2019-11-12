6e115f616cf899636f2fe59d016015e3b141a97a
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-11-11 23:13:49 -0600

    update

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Layout from "./hoc/layout";
 import Home from "./components/Home/index";
+import RegisterLogIn from "./components/register_log_in/index";
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+         <Route path="/register_log_in" component={RegisterLogIn} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
