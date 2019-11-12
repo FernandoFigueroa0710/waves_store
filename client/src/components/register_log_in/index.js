72501e39edbbcbe632888e02a2136758900ff7bd
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-11-12 08:44:41 -0600

    update settings for prettier

--- client/src/components/register_log_in/index.js
+++ client/src/components/register_log_in/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from "react";
 import MyButton from "../utils/button";
 
 const RegisterLogin = () => {
   return (
     <div className="page_wrapper">
-      <div className='container'>
-      <div className="register_login_container">
-        <div className="left">
-        <h1>New Customers</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-          <MyButton
-          type="default"
-          title="Create an account"
-          linkTo="/register"
-          addStyles={{
-            margin: '10px 0 0 0'
-          }}
-          />
+      <div className="container">
+        <div className="register_login_container">
+          <div className="left">
+            <h1>New Customers</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat
+            </p>
+            <MyButton
+              type="default"
+              title="Create an account"
+              linkTo="/register"
+              addStyles={{
+                margin: "10px 0 0 0",
+              }}
+            />
+          </div>
+          <div className="right"></div>
         </div>
-        <div className="right">
-
-        </div>
-      </div>
       </div>
     </div>
   );
--- client/src/components/utils/button.js
+++ client/src/components/utils/button.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-
-const MyButton = (props) => {
-
+const MyButton = props => {
   const buttons = () => {
-
-    let template ='';
-    switch(props.type){
-      case 'default':
-      template = <Link
-      className='link_default'
-      to={props.linkTo}
-      {...props.addStyles}
-      >
-        {props.title}
-      </Link>
-      break;
+    let template = "";
+    switch (props.type) {
+      case "default":
+        template = (
+          <Link className="link_default" to={props.linkTo} {...props.addStyles}>
+            {props.title}
+          </Link>
+        );
+        break;
       default:
-        template="";
+        template = "";
     }
 
     return template;
-  }
+  };
 
-  return (
-    <div className="my_link">
-            {buttons()}
-    </div>
-  );
+  return <div className="my_link">{buttons()}</div>;
 };
 
 export default MyButton;
