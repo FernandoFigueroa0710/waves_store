7a30f577b0e3e5fc9339a17080935df97601aedd
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-12-02 23:23:46 -0600

    update

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -5,18 +5,39 @@ import {CircularProgress} from "@material-ui/core";
 
 export default function(ComposedClass, reload, adminRoute = null) {
     class AuthenticationCheck extends Component {
+        _isMounted = false;
 
         state ={
-            loading: false,
+            loading: true,
 
         }
 
-        componentDidMount(){
+        componentDidMount() {
+        this._isMounted = true;
+
             this.props.dispatch(auth()).then(response => {
                 let user = this.props.user.userData;
-            })
+                if (!user.isAuth) {
+                    if (reload) {
+                        this.props.history.push("/register_log_in");
+                    }
+                } else {
+                    if(adminRoute && !user.isAdmin){
+                        this.props.history.push("/user/dashboard");
+                    } else {
+                        if (reload === false) {
+                            this.props.history.push("/user/dashboard");
+                        }
+                    }
+                }
+                this.setState({
+                    loading: false
+                })
+            });
         }
-
+        componentWillUnmount(){
+            this._isMounted = false;
+        }
         render() {
             if(this.state.loading){
                 return (
