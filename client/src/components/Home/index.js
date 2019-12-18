d97fe30acf5c65f538cd274c9a582d9774ecb746
Fernando Figueroa <fernandoffigueroa@gmail.com>
2019-12-17 20:50:58 -0600

    mapping card done

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import HomeSlider from "./home_slider";
 import HomePromotion from "./home_promotions";
+import CardBlock from "../utils/card_block";
 import { connect } from "react-redux";
 import {
     getProductsBySale,
@@ -15,7 +16,15 @@ class Home extends Component {
         return (
             <div>
                 <HomeSlider />
+                <CardBlock
+                    list={this.props.products.bySale}
+                    title="Best Selling Guitars"
+                />
                 <HomePromotion />
+                <CardBlock
+                    list={this.props.products.byArrival}
+                    title="New Arrivals"
+                />
             </div>
         );
     }
@@ -23,7 +32,7 @@ class Home extends Component {
 
 const mapStateToProps = state => {
     return {
-        products: state.props,
+        products: state.products,
     };
 };
 
