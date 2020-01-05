2246e91255790d101880be22a981951ce7bb1c65
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-01-04 18:04:36 -0600

    Server rendering items from shop endpoint

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,14 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.Promise = global.Promise;
 mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log("Mongo DB Connected"))
-  .catch(err => console.log("Err is", err));
-
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("Mongo DB Connected"))
+    .catch(err => console.log("Err is", err));
 
 //******MODELS****************//
 const { User } = require("./models/user");
@@ -33,159 +32,197 @@ const { Product } = require("./models/product");
 const { auth } = require("./middleware/auth");
 const { admin } = require("./middleware/admin");
 
-
-
-//************PRODUCT***********//
+//************PRODUCTS***********//
+//Get products to shop filters
 //Get by arrival
 //items?sortBy=createdAt&order=desc&limit=4
 
 // by sell
 //items?sortyBy=sold&order=desc&limit100&skip=5
 
-app.get("/api/products/items", (req, res) => {
-  let order = req.query.order ? req.query.order : "asc";
-  let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+app.post("/api/products/shop", (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 30;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+    let findArgs = {};
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1],
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+    Product.find(findArgs)
+        .populate("brand")
+        .populate("wood")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, items) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).send(items);
+        });
+});
 
-  Product.find()
-    .populate("brand")
-    .populate("wood")
-    .sort([[sortBy, order]])
-    .limit(limit)
-    .exec((err, items) => {
-      if (err) return res.status(400).send(err);
-      res.send(items);
-    });
+app.get("/api/products/items", (req, res) => {
+    let order = req.query.order ? req.query.order : "asc";
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.find()
+        .populate("brand")
+        .populate("wood")
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, items) => {
+            if (err) return res.status(400).send(err);
+            res.send(items);
+        });
 });
 
 app.get("/api/product/item_by_id", (req, res) => {
-  let type = req.query.type;
-  let items = req.query.id;
+    let type = req.query.type;
+    let items = req.query.id;
 
-  if (type === "array") {
-    let ids = req.query.id.split(",");
-    items = [];
-    items = ids.map(item => {
-      return mongoose.Types.ObjectId(item);
-    });
-  }
-  Product.find({ _id: { $in: items } })
-    .populate("brand")
-    .populate("wood")
-    .exec((err, docs) => {
-      if (err) return res.status(400);
-      res.status(200).send(docs);
-    });
+    if (type === "array") {
+        let ids = req.query.id.split(",");
+        items = [];
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item);
+        });
+    }
+    Product.find({ _id: { $in: items } })
+        .populate("brand")
+        .populate("wood")
+        .exec((err, docs) => {
+            if (err) return res.status(400);
+            res.status(200).send(docs);
+        });
 });
 
 app.post("/api/product/item", auth, admin, (req, res) => {
-  const product = new Product(req.body);
+    const product = new Product(req.body);
 
-  product.save((err, doc) => {
-    if (err) return res.json({ success: false, err });
-    res.status(200).json({
-      success: true,
-      product: doc,
+    product.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            product: doc,
+        });
     });
-  });
 });
 
 //***********WOODS*************//
 app.post("/api/product/wood", auth, admin, (req, res) => {
-  const wood = new Wood(req.body);
+    const wood = new Wood(req.body);
 
-  wood.save((err, doc) => {
-    if (err) return res.jason({ success: false, err });
-    res.status(200).json({
-      success: true,
-      wood: doc,
+    wood.save((err, doc) => {
+        if (err) return res.jason({ success: false, err });
+        res.status(200).json({
+            success: true,
+            wood: doc,
+        });
     });
-  });
 });
 
 app.get("/api/product/wood", (req, res) => {
-  Wood.find({}, (err, woods) => {
-    if (err) return res.status(400).send(err);
-    res.status(200).send(woods);
-  });
+    Wood.find({}, (err, woods) => {
+        if (err) return res.status(400).send(err);
+        res.status(200).send(woods);
+    });
 });
 //******Brands*****************//
 app.post("/api/product/brand", auth, admin, (req, res) => {
-  const brand = new Brand(req.body);
+    const brand = new Brand(req.body);
 
-  brand.save((err, doc) => {
-    if (err) return res.jason({ success: false, err });
-    res.status(200).json({
-      sucess: true,
-      brand: doc,
+    brand.save((err, doc) => {
+        if (err) return res.jason({ success: false, err });
+        res.status(200).json({
+            sucess: true,
+            brand: doc,
+        });
     });
-  });
 });
 
 app.get("/api/product/brands", (req, res) => {
-  Brand.find({}, (err, brands) => {
-    if (err) return res.status(400).send(err);
-    res.status(200).send(brands);
-  });
+    Brand.find({}, (err, brands) => {
+        if (err) return res.status(400).send(err);
+        res.status(200).send(brands);
+    });
 });
 //********USERS*****************//
 
 app.get("/api/users/auth", auth, (req, res) => {
-  res.status(200).json({
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastName: req.user.lastName,
-    role: req.user.role,
-    cart: req.user.cart,
-    history: req.user.history,
-  });
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastName: req.user.lastName,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history,
+    });
 });
 
 app.post("/api/users/register", (req, res) => {
-  const user = new User(req.body);
+    const user = new User(req.body);
 
-  user.save((err, doc) => {
-    if (err) return res.json({ success: false, err });
-    res.status(200).json({
-      success: true,
-      userdata: doc,
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            userdata: doc,
+        });
     });
-  });
 });
 
 app.post("/api/users/login", (req, res) => {
-  //find the email
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user)
-      return res.json({ loginSuccess: false, message: "Email not found" });
-    //check the password
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({ loginSuccess: false, message: "Wrong password" });
-      //generate a token
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
-        res.cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, message: "You are logged in", x_auth: user.token });
-      });
+    //find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Email not found",
+            });
+        //check the password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "Wrong password",
+                });
+            //generate a token
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        message: "You are logged in",
+                        x_auth: user.token,
+                    });
+            });
+        });
     });
-  });
 });
 
-
 app.get("/api/users/logout", auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: " " }, (err, doc) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).send({
-      sucess: true,
+    User.findOneAndUpdate({ _id: req.user._id }, { token: " " }, (err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+            sucess: true,
+        });
     });
-  });
 });
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+    console.log(`Server running at port ${port}`);
 });
