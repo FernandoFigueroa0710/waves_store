ddcf1750d60f31c55a67b70869aeb3f5f5e82881
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-04 19:54:12 -0600

    Update User nfo component basic set up with skelleton done

---
+++ client/src/components/User/admin/manage_site.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardLayout from "../../../hoc/dashboardLayout";
+import UpdateNfo from "./update_site.component";
+
+const ManageSite = () => {
+    return (
+        <DashboardLayout>
+            <UpdateNfo />
+        </DashboardLayout>
+    );
+};
+
+export default ManageSite;
---
+++ client/src/components/User/admin/update_site.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class UpdateNfo extends Component {
+    render() {
+        return (
+            <div>
+                <span>UPDATE NFO</span>
+            </div>
+        );
+    }
+}
+export default UpdateNfo;
