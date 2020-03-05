8a7994cd8c0908b1394335e221678162b65da143
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-04 23:00:19 -0600

    SITE INFO REDUX Created the skelleton in redux to update

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import user from "./user_reducer";
 import products from "./products_reducer";
+import siteNfo from "./site_reducer";
+
 const rootReducer = combineReducers({
     user,
     products,
+    siteNfo,
 });
 
 export default rootReducer;
