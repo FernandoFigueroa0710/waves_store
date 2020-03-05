5423745961be08989746bf6e5668743369769609
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-04 23:09:01 -0600

    Site address component Skelleton jsx set up for component

--- client/src/components/User/admin/update_site.component.jsx
+++ client/src/components/User/admin/update_site.component.jsx
@@ -10,10 +10,145 @@ import {
 
 import { connect } from "react-redux";
 class UpdateNfo extends Component {
+    state = {
+        formError: false,
+        formSuccess: false,
+        formData: {
+            address: {
+                element: "input",
+                value: "",
+                config: {
+                    label: "Address",
+                    name: "address_input",
+                    type: "text",
+                    placeholder: "Enter the site address",
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: "",
+                showLabel: true,
+            },
+            hours: {
+                element: "input",
+                value: "",
+                config: {
+                    label: "Working hours",
+                    name: "hours_input",
+                    type: "text",
+                    placeholder: "Enter working hours",
+                },
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: "",
+                showLabel: true,
+            },
+            phone: {
+                element: "input",
+                value: "",
+                config: {
+                    label: "Phone Number",
+                    name: "phone_input",
+                    type: "number",
+                    placeholder: "Enter site number",
+                },
+                validation: {
+                    required: true,
+                    email: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: "",
+                showLabel: true,
+            },
+            email: {
+                element: "input",
+                value: "",
+                config: {
+                    label: "Store email",
+                    name: "email_input",
+                    type: "email",
+                    placeholder: "Enter store email",
+                },
+                validation: {
+                    required: true,
+                    email: true,
+                },
+                valid: false,
+                touched: false,
+                validationMessage: "",
+                showLabel: true,
+            },
+        },
+    };
+    updateForm = element => {
+        const newFormData = update(element, this.state.formData, "site_info");
+        this.setState({
+            formError: false,
+            formData: newFormData,
+        });
+    };
+    submitForm = event => {
+        event.preventDefault();
+
+        let dataToSubmit = generateData(this.state.formData, "site_info");
+
+        let formIsValid = isFormValid(this.state.formData, "site_info");
+
+        if (formIsValid) {
+            console.log("HERE", dataToSubmit);
+        } else {
+            this.setState({
+                formError: true,
+            });
+        }
+    };
     render() {
         return (
             <div>
-                <span>UPDATE NFO</span>
+                <form onSubmit={event => this.submitForm(event)}>
+                    <h1>Store info</h1>
+                    <FormField
+                        id={"address"}
+                        formData={this.state.formData.address}
+                        change={element => this.updateForm(element)}
+                    />
+                    <FormField
+                        id={"hours"}
+                        formData={this.state.formData.hours}
+                        change={element => this.updateForm(element)}
+                    />
+                    <FormField
+                        id={"phone"}
+                        formData={this.state.formData.phone}
+                        change={element => this.updateForm(element)}
+                    />
+                    <FormField
+                        id={"email"}
+                        formData={this.state.formData.email}
+                        change={element => this.updateForm(element)}
+                    />
+                    <div>
+                        {this.state.formSuccess ? (
+                            <div className="form_success">
+                                Store info updated
+                            </div>
+                        ) : null}
+                        {this.state.formError ? (
+                            <div className="error_label">
+                                Please check your data
+                            </div>
+                        ) : null}
+                        <button onClick={event => this.submitForm(event)}>
+                            Update store address
+                        </button>
+                    </div>
+                </form>
             </div>
         );
     }
