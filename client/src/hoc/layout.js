79b5bdec963d7f22d7bb4d7e889e3bf224a9e11d
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-04-11 14:54:48 -0500

    SITE INFO Footer rendering the info dynamically

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import Header from "../components/header_footer/header/index";
 import Footer from "../components/header_footer/footer/index";
+
+import { connect } from "react-redux";
+import { getSiteData } from "../redux/actions/site_actions";
 class Layout extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="page_container">{this.props.children}</div>
-        <Footer />
-      </div>
-    );
-  }
+    componentDidMount() {
+        if (Object.keys(this.props.site).length === 0) {
+            this.props.dispatch(getSiteData());
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="page_container">{this.props.children}</div>
+                <Footer data={this.props.site} />
+            </div>
+        );
+    }
 }
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        site: state.siteNfo,
+    };
+};
+export default connect(mapStateToProps)(Layout);
